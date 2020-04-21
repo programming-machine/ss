7a84ed365c969150aa4efe6e2c9a5c1bb55c8ce8
sina <sinaali.7.70@gmail.com>
2020-04-20 19:17:01 -0700

    new changes

--- client/client.js
+++ client/client.js
@@ -2,14 +2,22 @@ console.log('hellow world');
 
 const form = document.querySelector('form');
 const loadingElement = document.querySelector('.loading');
+const API_URL = "http://localhost:5000/mytwitter";
+
+
 loadingElement.style.display = 'none';
+
+function eraseText() {
+    document.getElementById("name").value = "";
+    document.getElementById("content").value = "";
+}
 
 form.addEventListener('submit', (event) => {
     event.preventDefault(); // prevents the text to get disappeared from the console
     //console.log('form submited!');
     const formData = new FormData(form);
     const name = formData.get('name');
-    const content = formData.get('comment');
+    const content = formData.get('content');
 
     const newComment = {
         name,
@@ -19,10 +27,14 @@ form.addEventListener('submit', (event) => {
 
     form.style.display = 'none';
     loadingElement.style.display = '';
+
+    fetch(API_URL, {
+        method: 'post',
+        body: JSON.stringify(newComment),
+        headers: {
+            'content-type': 'application/json'
+        }
+
+    });
     eraseText();
 });
-
-function eraseText() {
-    document.getElementById("name").value = "";
-    document.getElementById("content").value = "";
-}
--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
 
 app.listen(5000, () => {
     console.log('Listening on http://localhost:5000');
@@ -8,6 +14,28 @@ app.listen(5000, () => {
 
 app.get('/', (req, res) => {
     res.json({
-        message: 'posted'
+        message: 'posted !!!!!'
     });
+});
+
+
+function isVAlidComment(comment) {
+    return comment.name && comment.name.toString().trim() !== '' &&
+        comment.content && comment.content.toString().trim() !== '';
+}
+
+app.post('/mytwitter', (req, res) => {
+    if (isVAlidComment(req.body)) {
+        const comment = {
+            name: req.body.name.toString(),
+            content: req.body.content.toString()
+
+        };
+        console.log(comment);
+    } else {
+        res.status(422);
+        res.json({
+            message: "name and content are required"
+        });
+    }
 });
